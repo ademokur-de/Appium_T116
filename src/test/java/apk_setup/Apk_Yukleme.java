a8ef176da51ba5944c3be8d5f30c16009211c421
AdemOkurDE <ademokur.tr@gmail.com>
2023-08-20 19:48:49 +0200

    23/08/20-V2

---
+++ src/test/java/apk_setup/Apk_Yukleme.java
@@ -0,0 +1,35 @@
+package apk_setup;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class Apk_Yukleme {
+
+    AndroidDriver<AndroidElement> driver; //android cihazlarin driveri
+    final String cihazName="Pixel 2";
+    final String platfomVersion="8.1";
+
+    @Test
+    public void ApkYukle() throws MalformedURLException {
+
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,cihazName);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,platfomVersion);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        //capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ademo\\IdeaProjects\\APPIUM_T-116\\Apps\\Apk Bilgisi_2.3.4_apkcombo.com.apk");
+        //capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ademo\\IdeaProjects\\APPIUM_T-116\\Apps\\arabam.com_4.8.0_Apkpure.apk");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ademo\\IdeaProjects\\APPIUM_T-116\\Apps\\Kiwi.com - Book Cheap Flights_2023.14.0_Apkpure.apk");
+
+
+        driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+}
--- src/test/java/day2/arabamCom.java
+++ src/test/java/day2/arabamCom.java
@@ -1,14 +1,19 @@
 package day2;
 
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class arabamCom {
@@ -17,7 +22,7 @@ public class arabamCom {
     @BeforeTest
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities=new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"PIXEL");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 2");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"8.1");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
@@ -36,24 +41,90 @@ public class arabamCom {
 
 
     @Test
-    public void arabamTest(){
+    public void arabamTest() throws InterruptedException {
         //  driver.activateApp("com.dogan.arabam"); Bir uygulamayi akrif hale getirmek istedigimizde apk info dan onun bundleId degerini bularak buradan aktif hale getirebiliriz
 
+        Assert.assertTrue(driver.isAppInstalled("com.dogan.arabam"));
+
+
+        AndroidElement headerKontrol=driver.findElementByXPath("//*[@text='Arabam kaç para?']");
+                // driver.findElementById("com.dogan.arabam:id/tvShowroomInfo"); // burasri benim tel de olmadi Android 8.1 oldugundan olabilir!!
+        Assert.assertTrue(headerKontrol.isDisplayed());
 
         // Arabam kac para bolumune tiklayalim
+        driver.findElementById("com.dogan.arabam:id/tvPricePrediction").click();
+        // ya da xpath ile :
+        // driver.findElementByXPath("//*[@text='Arabam kaç para?']");
+        // Appium da Text attribiute'u ile calisirken "@" isareti kullanilir cift parantezler "()" kullanilmaz!!!
+
         // Aracimin fiyatini merak ediyorum bolumune tiklayalim
-        // Wolkswagen markasini secelim
+        driver.findElementByXPath("//*[@text='Aracımın fiyatını merak ediyorum']").click();
+
+        // Volkswagen markasini secelim
+        Thread.sleep(1500);
+        TouchAction action = new TouchAction<>(driver);
+        //alt : 535,1726   üst : 535, 240
+
+        action.press(PointOption.point(535,1726))
+              .waitAction(WaitOptions.waitOptions(Duration.ofMillis(500)))
+              .moveTo(PointOption.point(535,240))
+              .release()
+              .perform();
+
+        driver.findElementByXPath("//*[@text='Volkswagen']").click();
+
         // yil secimi yapalim
+        driver.findElementByXPath("//*[@text='2018']").click();
+
         // model secimi yapalim
+        driver.findElementByXPath("//*[@text='Passat']").click();
+
         // govde tipini secelim
+        driver.findElementByXPath("//*[@text='Sedan']").click();
+
         // yakit tipini secelim
+        driver.findElementByXPath("//*[@text='Benzin']").click();
+
         // vites tipini secelim
+        driver.findElementByXPath("//*[@text='Yarı Otomatik']").click();
+
         // Versiyon secimi yapalim
+        Thread.sleep(1000);
+        action.press(PointOption.point(456,618)).release().perform();
+
         // aracin km bilgilerini girelim
+        AndroidElement kmBox=driver.findElementById("com.dogan.arabam:id/et_km");
+
+        if(driver.isKeyboardShown()){
+            driver.getKeyboard().pressKey("150000");
+        }else {
+            kmBox.sendKeys("200000");
+        }
+        Thread.sleep(1000);
+        driver.findElementByXPath("//*[@text='Devam']").click();
+
         // aracin rengini secelim
-        // opsiyel donanim (varsa) seecelim
+        driver.findElementByXPath("(//*[@class='android.widget.TextView'])[16]").click();
+
+        // opsiyel donanim (varsa) secelim
+        driver.findElementByXPath("//*[@text='Devam']").click();
+        Thread.sleep(1000);
         // degisen bilgisi ekleyerek tramer kaydi belirtelim
+        action.press(PointOption.point(538,795)).release().perform();
+        action.press(PointOption.point(230,1619)).release().perform();
+        Thread.sleep(1000);
+        driver.findElementByXPath("//*[@text='Devam']").click();
+
+        Thread.sleep(1000);
+        driver.findElementByXPath("//*[@text='Tramer kaydı yok']").click();
+        driver.findElementByXPath("//*[@text='Devam']").click();
+
         // aracimizin fiyatinin 500.000 tl den fazla oldugunu test edelim
+        AndroidElement ortalamaFiyatSonucuLocate=driver.findElementById("com.dogan.arabam:id/tvAveragePrice");
+        String ortalamaSonSonuc=ortalamaFiyatSonucuLocate.getText();
+        ortalamaSonSonuc=ortalamaSonSonuc.replaceAll("\\D","");
+
+        Assert.assertTrue(Integer.parseInt(ortalamaSonSonuc)>5000);
         // uygulamayi kapatalim
     }
 
